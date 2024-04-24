035b09eb9ef69a24399c6ebd4372c811f90946bb
yakushev <ugr@bk.ru>
2024-04-24 13:18:14 +0500

    In methods of object QueryParDivider type Query replaced with type parameter A.
    A <: Query

--- ora_ch_app/src/main/scala/calc/QueryParDivider.scala
+++ ora_ch_app/src/main/scala/calc/QueryParDivider.scala
@@ -6,21 +6,21 @@ object QueryParDivider {
 
   private def listToQueue[A](l: List[A]): mutable.Queue[A] = new mutable.Queue[A] ++= l
 
-  def mapOfQueues(queries: List[Query]): Map[Int, mutable.Queue[Query]] =
-    queries.groupBy(_.query_id).map { case (k: Int, lst: List[Query]) =>
+  def mapOfQueues[A <: Query](queries: List[A]): Map[Int, mutable.Queue[A]] =
+    queries.groupBy(_.query_id).map { case (k: Int, lst: List[A]) =>
       (k, listToQueue(lst))
     }
 
-  def listOfListsQuery(
-                        m: Map[Int, mutable.Queue[Query]],
-                        acc: List[List[Query]] = List.empty[List[Query]]
-                      ): List[List[Query]] =
+  def listOfListsQuery[A](
+                        m: Map[Int, mutable.Queue[A]],
+                        acc: List[List[A]] = List.empty[List[A]]
+                      ): List[List[A]] =
     if (m.exists(_._2.nonEmpty)) {
       if (m.count(_._2.nonEmpty) == 1) {
         val nonemptyQueue = m.find(_._2.nonEmpty)
         nonemptyQueue match {
           case Some((key, queue)) =>
-            val element: Query = queue.dequeue()
+            val element: A = queue.dequeue()
             listOfListsQuery(Map(key -> queue), acc :+ List(element))
           case None               => acc
         }
@@ -28,8 +28,8 @@ object QueryParDivider {
         val keys: List[Int]          = m.filter(_._2.nonEmpty).keys.toList
         val k1: Int                  = keys.head
         val k2: Int                  = keys.tail.head
-        val q1: mutable.Queue[Query] = m.getOrElse(k1, mutable.Queue.empty[Query])
-        val q2: mutable.Queue[Query] = m.getOrElse(k2, mutable.Queue.empty[Query])
+        val q1: mutable.Queue[A] = m.getOrElse(k1, mutable.Queue.empty[A])
+        val q2: mutable.Queue[A] = m.getOrElse(k2, mutable.Queue.empty[A])
         val query1                   = q1.dequeue()
         val query2                   = q2.dequeue()
         listOfListsQuery(m.updated(k1, q1).updated(k2, q2), acc :+ List(query1, query2))
